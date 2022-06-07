6eed8d5a50d119a229e1379178d014616ba42c92
osama24sy <79219780+osama24sy@users.noreply.github.com>
2022-06-07 15:35:19 +0300

    Finish first protoype of the game

---
+++ script.js
@@ -0,0 +1,74 @@
+const checkWinner = (board) => {
+    for (let i = 0; i < 3; i++) {
+        // check vertically
+        if (board[i][0] == board[i][1] && board[i][1] == board[i][2]) {
+            return {win: true, winner: board[i][0]};
+        }
+        // check horizontally
+        if (board[0][i] == board[1][i] && board[1][i] == board[2][i]) {
+            return {win: true, winner: board[0][i]};
+        }
+    }
+    // check diagonally
+    if ((board[0][0] == board[1][1]) && (board[1][1] == board[2][2])) {
+        return {win: true, winner: board[0][0]};
+    } else if (board[0][2] == board[1][1] && board[1][1] == board[2][0]) {
+        return {win: true, winner: board[0][2]};
+    }
+    return {win: false, winner: null};
+}
+
+const findLocation = (location) => {
+    let row = Math.floor(location/3);
+    if (location%3 == 0) {
+        row--;
+    }
+    let column = 0;
+    if (location%3 == 0) {
+         column = 2;
+    } else {
+         column = location%3 - 1;
+    }
+    return {row, column};
+}
+
+const changeMatrix = (location, board, sign) => {
+    const coordinates = findLocation(location);
+    board[coordinates.row][coordinates.column] = sign;
+}
+
+// Game Matrix
+let board = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+];
+
+let round = 1;
+
+const boxes = document.querySelectorAll(".box");
+const result = document.querySelector(".winner");
+
+boxes.forEach(box => {
+    box.addEventListener("click", () => {
+        if (round%2 == 1 && !box.hasChildNodes()) {
+            const p = document.createElement("p");
+            p.classList.add("big-text");
+            p.textContent = "X";
+            box.appendChild(p);
+            changeMatrix(box.id, board, "X");
+            round++;
+        } else if (round%2 == 0 && !box.hasChildNodes()) {
+            const p = document.createElement("p");
+            p.classList.add("big-text");
+            p.textContent = "O";
+            box.appendChild(p);
+            changeMatrix(box.id, board, "O");
+            round++;
+        }
+        const winner = checkWinner(board);
+        if (winner.win) {
+            result.textContent = `Winner is ${winner.winner}`;
+        }
+    });
+});
